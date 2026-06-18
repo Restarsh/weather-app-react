058c398c3a9d5a88899c2f2c08189bfdae4fdbcd
Amir Shrestha <amirshrestha2006@gmail.com>
2026-06-18 21:51:18 +0545

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
-import Weather from './components/weather';
+import Weather from './components/Weather';
 export default function App() {
 
   const [lat, setLat] = useState([]);
